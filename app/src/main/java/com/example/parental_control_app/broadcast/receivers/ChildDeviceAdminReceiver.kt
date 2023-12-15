abae831e4e746462e6fc749197690726f0fa76b1
Francis James Tolentino <francistolentino1107@gmail.com>
2023-12-15 20:59:16 +0800

    Revisions

--- app/src/main/java/com/example/parental_control_app/broadcast/receivers/ChildDeviceAdminReceiver.kt
+++ app/src/main/java/com/example/parental_control_app/broadcast/receivers/ChildDeviceAdminReceiver.kt
@@ -5,25 +5,10 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.core.content.ContextCompat
 import com.example.parental_control_app.managers.SharedPreferencesManager
-import com.example.parental_control_app.repositories.users.UserProfile
-import com.example.parental_control_app.repositories.users.UsersRepository
 import com.example.parental_control_app.service.UninstallationService
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 
 class ChildDeviceAdminReceiver : DeviceAdminReceiver() {
 
